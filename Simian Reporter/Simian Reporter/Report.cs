caf8f3d1d4edaef4d883821f2909cbbaa0186279
Nathan Jervis <mirhagk@gmail.com>
2014-01-05 16:15:16 -0500

    Parses simian file now
    Parses sets with blocks and summary as well. Now that parsing is done,
    just need some analysis and to output the report.

--- Simian Reporter/Simian Reporter/Report.cs
+++ Simian Reporter/Simian Reporter/Report.cs
@@ -16,16 +16,53 @@ namespace Simian_Reporter
                 public string SourceFile { get; set; }
                 public int StartLine { get; set; }
                 public int EndLine { get; set; }
+                public Block(XmlNode node)
+                {
+                    SourceFile = node.Attributes["sourceFile"].Value;
+                    StartLine = node.AttributeAsInt("startLineNumber");
+                    EndLine = node.AttributeAsInt("endLineNumber");
+                }
+            }
+            public int LineCount { get; set; }
+            public Block[] Blocks { get; set; }
+            public Set(XmlNode node)
+            {
+                LineCount = int.Parse(node.Attributes["lineCount"].Value);
+                List<Block> blocks = new List<Block>();
+                foreach (XmlNode child in node.ChildNodes)
+                {
+                    blocks.Add(new Block(child));
+                }
+                Blocks = blocks.ToArray();
             }
         }
         public class Summary
         {
+            public int DuplicateFileCount{get;set;}
+            public int DuplicateLineCount{get;set;}
+            public int DuplicateBlockCount{get;set;}
+            public int TotalFileCount{get;set;}
+            public int TotalRawLineCount{get;set;}
+            public int TotalSignificantLineCount{get;set;}
+            public int ProcessingTime{get;set;}
+            public Summary(XmlNode node)
+            {
+                DuplicateFileCount = node.AttributeAsInt("duplicateFileCount");
+                DuplicateLineCount = node.AttributeAsInt("duplicateLineCount");
+                DuplicateBlockCount = node.AttributeAsInt("duplicateBlockCount");
+                TotalFileCount = node.AttributeAsInt("totalFileCount");
+                TotalRawLineCount = node.AttributeAsInt("totalRawLineCount");
+                TotalSignificantLineCount = node.AttributeAsInt("totalSignificantLineCount");
+                ProcessingTime = node.AttributeAsInt("processingTime");
+            }
 
         }
         public class Rules
         {
 
         }
+        public Set[] Sets { get; set; }
+        public Summary summary { get; set; }
         private static string CleanXML(string xml)
         {
             int index = xml.IndexOf("<?xml");
@@ -37,11 +74,24 @@ namespace Simian_Reporter
             XmlDocument document = new XmlDocument();
             document.LoadXml(xml);
 
+            List<Set> sets = new List<Set>();
+            Summary summary = null;
             foreach (XmlNode node in document.FirstElementChild().FirstChild.ChildNodes)
             {
+                switch (node.Name)
+                {
+                    case "set":
+                        sets.Add(new Set(node));
+                        break;
+                    case "summary":
+                        summary = new Summary(node);
+                        break;
+                    default:
+                        break;
+                }
                 Console.WriteLine(node.Name);
             }
-            return new Report();
+            return new Report() { Sets = sets.ToArray(), summary = summary };
         }
     }
 }
