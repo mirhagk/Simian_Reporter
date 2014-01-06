5c42e4729e92f6b73aa3346ca3a23aaf4bb52f14
Nathan Jervis <mirhagk@gmail.com>
2014-01-05 21:48:19 -0500

    Added LaTeX formatter
    Has replacement strings to escape LaTeX symbols

---
+++ Simian Reporter/Simian Reporter/LatexFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simian_Reporter
+{
+    class LatexFormatter
+    {
+        public Dictionary<string, string> replacementStrings = new Dictionary<string, string>()
+        {
+            {@">", @"\textrangle "},
+            {@"<", @"\textlangle "},
+            {@"\", @"\textbackslash "},
+            {@"_", @"\_"}
+        };
+        public static string GenerateReport(Report report)
+        {
+        }
+    }
+}
